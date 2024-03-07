75f2bfc1c9e95c0410a81465fb39afc95ada74e9
Denta Bramasta Hidayat <denta.dent53@gmail.com>
2024-03-07 19:38:29 +0100

    add kubernetes

--- AmazinGateaway/Program.cs
+++ AmazinGateaway/Program.cs
@@ -15,8 +15,7 @@ app.UseCors(builder => builder
     .AllowAnyMethod()
     .AllowAnyHeader()
 );
+app.MapGet("/test", () => "Hello, Denta, Welcome Back!");
 
-await app.UseOcelot();
-app.Run();
 await app.UseOcelot();
 app.Run();
