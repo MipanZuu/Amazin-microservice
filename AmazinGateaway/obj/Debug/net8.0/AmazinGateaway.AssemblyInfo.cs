14a29f0e724579763919568f941c1762d8053f9e
Denta Bramasta Hidayat <denta.dent53@gmail.com>
2024-03-07 16:28:00 +0100

    fix issue docker registry

--- AmazinGateaway/obj/Debug/net8.0/AmazinGateaway.AssemblyInfo.cs
+++ AmazinGateaway/obj/Debug/net8.0/AmazinGateaway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmazinGateaway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99263f152f1f52e79dda1602c5225007fb22480f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c30e3d4c23fc15cf61477698ffc3b85933d587d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmazinGateaway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmazinGateaway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
