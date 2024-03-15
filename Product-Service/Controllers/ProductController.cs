087a7b3731cd219db2732552aabe562e8bf9a148
Denta Bramasta Hidayat <denta.dent53@gmail.com>
2024-03-15 16:35:29 +0100

    issue with service interface

--- Product-Service/Controllers/ProductController.cs
+++ Product-Service/Controllers/ProductController.cs
@@ -2,69 +2,66 @@ using Microsoft.AspNetCore.Mvc;
 using Product_Service.Models;
 using Product_Service.Services;
 
-namespace Product_Service.Controllers
+namespace Product_Service.Controllers;
+
+
+[ApiController]
+[Route("/api/[controller]")]
+public class ProductController : ControllerBase
 {
-    [ApiController]
-    [Route("/api/[controller]")]
-    public class ProductController : ControllerBase
+    private readonly IProductService _productService;
+    public ProductController(IProductService productService) => _productService = productService;
+
+    [HttpGet("{id:length(24)}")]
+    public async Task<IActionResult> Get(string id)
     {
-        private readonly IProductService _productService;
-
-        public ProductController(IProductService productService)
-        {
-            _productService = productService;
-        }
-
-        [HttpGet("{id:length(24)}")]
-        public async Task<IActionResult> Get(string id)
-        {
-            var existingProduct = await _productService.GetAsync(id);
-            if (existingProduct is null)
-                return NotFound();
-
-            return Ok(existingProduct);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetAllProducts()
-        {
-            var allProducts = await _productService.GetAsync();
-            if (allProducts.Any())
-                return Ok(allProducts);
-
+        var existingProduct = await _productService.GetAsync(id);
+        if (existingProduct is null)
             return NotFound();
-        }
 
-        [HttpPost]
-        public async Task<IActionResult> PostProduct(ProductModel productModel)
-        {
-            await _productService.CreateAsync(productModel);
-            return CreatedAtAction(nameof(Get), new { id = productModel.Id }, productModel);
-        }
+        return Ok(existingProduct);
+    }
 
-        [HttpPut("{id:length(24)}")]
-        public async Task<IActionResult> UpdateProduct(string id, ProductModel productModel)
-        {
-            var existingProduct = await _productService.GetAsync(id);
-            if (existingProduct is null)
-                return BadRequest();
+    [HttpGet]
+    public async Task<IActionResult> GetAllProducts()
+    {
+        var allProducts = await _productService.GetAsync();
+        if (allProducts.Any())
+            return Ok(allProducts);
 
-            productModel.Id = existingProduct.Id;
+        return NotFound();
+    }
+    
 
-            await _productService.UpdateAsync(productModel);
-            return Ok();
-        }
+    [HttpPost]
+    public async Task<IActionResult> PostProduct(ProductModel productModel)
+    {
+        await _productService.CreateAsync(productModel);
+        return CreatedAtAction(nameof(Get), new { id = productModel.Id }, productModel);
+    }
 
-        [HttpDelete("{id:length(24)}")]
-        public async Task<IActionResult> DeleteProduct(string id)
-        {
-            var existingProduct = await _productService.GetAsync(id);
-            if (existingProduct is null)
-                return BadRequest();
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> UpdateProduct(String id, ProductModel productModel)
+    {
+        var existingProduct = await _productService.GetAsync(id);
+        if (existingProduct is null)
+            return BadRequest();
 
-            await _productService.RemoveAsync(id);
+        productModel.Id = existingProduct.Id;
 
-            return NoContent();
-        }
+        await _productService.UpdateAsync(productModel);
+        return Ok();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> DeleteProduct(String id)
+    {
+        var existingProduct = await _productService.GetAsync(id);
+        if (existingProduct is null)
+            return BadRequest();
+
+        await _productService.RemoveAsync(id);
+
+        return NoContent();
     }
 }
